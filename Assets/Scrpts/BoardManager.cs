dee359303b398339976c2d535d49cfe6cc7ca41b
David Abruzzo <davidabruzzogames@gmail.com>
2025-05-31 08:26:55 +0200

    added passable boolean

--- Assets/Scrpts/BoardManager.cs
+++ Assets/Scrpts/BoardManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
-     private Tilemap m_tilemap;
+    private Tilemap m_tilemap;
 
     public int width = 10;
     public int height = 10;
@@ -14,29 +14,42 @@ public class BoardManager : MonoBehaviour
     void Start()
     {
         m_tilemap = GetComponentInChildren<Tilemap>();
+        m_BoardData = new CellData[width, height];
 
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
+
+                // Check if the current position is on the border of the tilemap
                 if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
                 {
                     // Set wall tiles on the borders
                     int tileNumber = Random.Range(0, WallTiles.Length);
                     tile = WallTiles[tileNumber];
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     // Set ground tiles inside the borders
                     int tileNumber = Random.Range(0, groundTiles.Length);
                     tile = groundTiles[tileNumber];
+                    m_BoardData[x, y].Passable = true;
                 }
-               m_tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                m_tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
 
         }
     }
+
+    public class CellData
+    {
+        public bool Passable{ get; set; }
+    }
+
+    private CellData[,] m_BoardData;
 }
 
 
